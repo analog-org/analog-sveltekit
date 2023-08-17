0778872d6fe43299fc41ebe76ad0f6d4e56ae612
BossDaily <gboys2008@gmail.com>
2023-08-17 14:36:30 -0500

    auth session handler

---
+++ apps/dashboard/src/+hooks.server.ts
@@ -0,0 +1,60 @@
+import { SvelteKitAuth } from "@auth/sveltekit";
+import Discord from "@auth/core/providers/discord";
+
+export const handle = SvelteKitAuth({
+  providers: [
+    Discord({
+      clientId: process.env.DISCORD_CLIENT_ID,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET,
+      authorization: {
+        params: {
+          scope: "identify guilds email connections",
+        },
+      },
+      profile(profile) {
+        if (profile.avatar === null) {
+          const defaultAvatarNumber = parseInt(profile.discriminator) % 5;
+          profile.image_url = `https://cdn.discordapp.com/embed/avatars/${defaultAvatarNumber}.png`;
+        } else {
+          const format = profile.avatar.startsWith("a_") ? "gif" : "png";
+          profile.image_url = `https://cdn.discordapp.com/avatars/${profile.id}/${profile.avatar}.${format}`;
+        }
+
+        return {
+          id: profile.id,
+          name: profile.username,
+          discriminator: profile.discriminator,
+          image: profile.image_url,
+          accentColor: profile.accentColor,
+        };
+      },
+    }),
+  ],
+  callbacks: {
+    jwt: async ({ token, account, profile }) => {
+      if (account) {
+        token.accessToken = account.access_token;
+        token.tokenType = account.token_type;
+      }
+      if (profile) {
+        token.profile = profile;
+      }
+      return token;
+    },
+    // @ts-ignore
+    session: async ({ session, token }) => {
+      // @ts-ignore
+      session.accessToken = token.accessToken;
+      // @ts-ignore
+      session.refreshToken = token.refreshToken;
+      // @ts-ignore
+      session.tokenType = token.tokenType;
+      // @ts-ignore
+      session.discordUser = token.profile;
+      // @ts-ignore
+
+      return session;
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+});
