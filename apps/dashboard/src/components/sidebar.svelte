4d8e0c9854fde5fbb7185fc71b631bac22cf2e50
BossDaily <gboys2008@gmail.com>
2023-12-24 14:58:33 -0600

    updated dependencies and installed shadcn-svelte

---
+++ apps/dashboard/src/components/sidebar.svelte
@@ -0,0 +1,16 @@
+<script>
+  let isOpen = true;
+  const toggleSidebar = () => {
+    isOpen = !isOpen;
+  };
+</script>
+
+<div class="flex h-screen bg-gray-200">
+  <div class={`bg-white w-64 absolute sm:relative  shadow p-5 ${isOpen ? 'translate-x-0 ease-out transition-medium' : '-translate-x-full ease-in transition-medium'}`}>
+    <p>40</p>
+    <button on:click={toggleSidebar} class="mb-5">Toggle Sidebar</button>
+  </div>
+  <div class="w-full ml-64 sm:ml-0">
+    <!-- Other content -->
+  </div>
+</div>
--- apps/dashboard/src/routes/+layout.svelte
+++ apps/dashboard/src/routes/+layout.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
     import type { LayoutData } from './$types';
     import "../app.css"
+    import "../components/sidebar.svelte"
     export let data: LayoutData;
 </script>
 
-<slot />
+<sidebar />
+<p>fortnite</p>
+<slot></slot>
