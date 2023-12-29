9a534344c490c2c9f9f6dfcc37674e96d1b80d7c
BossDaily <gboys2008@gmail.com>
2023-12-29 16:51:31 -0600

    flowbite installation

--- apps/dashboard/tailwind.config.js
+++ apps/dashboard/tailwind.config.js
@@ -3,8 +3,9 @@ import { fontFamily } from "tailwindcss/defaultTheme";
 /** @type {import('tailwindcss').Config} */
 const config = {
 	darkMode: ["class"],
-	content: ["./src/**/*.{html,js,svelte,ts}"],
+	content: ["./src/**/*.{html,js,svelte,ts}", "'./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}"],
   safelist: ["dark"],
+	plugins: [require('flowbite/plugin')],
 	theme: {
 		container: {
 			center: true,
