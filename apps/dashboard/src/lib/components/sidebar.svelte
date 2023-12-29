c979713f9579cb231dbbb2cc10ae1bac65f058bc
BossDaily <gboys2008@gmail.com>
2023-12-28 18:08:04 -0600

    updated sidebar import

--- apps/dashboard/src/lib/components/sidebar.svelte
+++ apps/dashboard/src/lib/components/sidebar.svelte
@@ -7,7 +7,6 @@
 
 <div class="flex h-screen bg-gray-200">
   <div class={`bg-white w-64 absolute sm:relative  shadow p-5 ${isOpen ? 'translate-x-0 ease-out transition-medium' : '-translate-x-full ease-in transition-medium'}`}>
-    <p>40</p>
     <button on:click={toggleSidebar} class="mb-5">Toggle Sidebar</button>
   </div>
   <div class="w-full ml-64 sm:ml-0">
--- apps/dashboard/src/routes/+layout.svelte
+++ apps/dashboard/src/routes/+layout.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
     import type { LayoutData } from './$types';
     import "../app.css"
-    import "$lib/components/sidebar.svelte"
+    import Sidebar from "$lib/components/sidebar.svelte"
     export let data: LayoutData;
 </script>
 
-<sidebar />
-<p>fortnite</p>
+<Sidebar />
+
 <slot></slot>
