b50bdf22081a48f50773c9ca4b4dd5a43fd334b7
BossDaily <gboys2008@gmail.com>
2024-01-01 14:38:37 -0600

    Integrates bot and user info into page data

--- apps/dashboard/src/routes/+layout.server.ts
+++ apps/dashboard/src/routes/+layout.server.ts
@@ -1,7 +1,33 @@
-import type { LayoutServerLoad } from "./$types"
+import { REST } from '@discordjs/rest';
+import type { LayoutServerLoad } from "./$types";
+import { Routes, type APIUser } from 'discord-api-types/v10'
+import { DISCORD_CLIENT_ID, DISCORD_CLIENT_SECRET, DISCORD_BOT_TOKEN} from "$env/static/private";
+
 
 export const load: LayoutServerLoad = async (event) => {
+  const currentSession = await event.locals.getSession();
+  
+  if (currentSession) {
+    //@ts-ignore
+    const accessToken = currentSession.accessToken;
+    //@ts-ignore
+    const discordUser: APIUser = currentSession.discordUser;
+
+    const userRest = new REST({authPrefix: 'Bearer'}).setToken(accessToken);
+    const botRest = new REST({authPrefix: 'Bot'}).setToken(DISCORD_BOT_TOKEN);
+
+    const userInfo = await userRest.get(Routes.user('@me'));
+    
+    const botInfo = await botRest.get(Routes.user('@me'));
+
+    return {
+      session: currentSession,
+      user: userInfo,
+      bot: botInfo,
+    }
+  }
+
   return {
-    session: await event.locals.getSession(),
+    session: currentSession,
   }
 }
