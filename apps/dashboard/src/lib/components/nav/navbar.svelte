ea984f6b5ab5bab6054d7f706f2a2dd6f9b59f3e
BossDaily <gboys2008@gmail.com>
2024-01-01 14:46:08 -0600

    removed user prop in navbar and added the bot to the navbar

--- apps/dashboard/src/lib/components/nav/navbar.svelte
+++ apps/dashboard/src/lib/components/nav/navbar.svelte
@@ -1,15 +1,15 @@
 <script lang="ts">
   import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, Avatar, Dropdown, DropdownItem, DropdownHeader, DropdownDivider } from 'flowbite-svelte';
   import { type APIUser } from 'discord-api-types/v10'
-  export let user: APIUser;
+  /* export let user: APIUser; */
   export let bot: APIUser;
   
 </script>
 
 <Navbar>
   <NavBrand href="/">
-    <img src="https://cdn.discordapp.com/avatars/966503302407802972/df76bc0a359726041417a2c3979489d7.png?size=1024&format=webp&quality=lossless&width=0&height=256" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" />
-    <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite</span>
+    <img src={`https://cdn.discordapp.com/avatars/${bot.id}/${bot.avatar}.png?size=1024&format=webp&quality=lossless&width=0&height=256`} class="me-3 h-6 sm:h-9" alt="Flowbite Logo" />
+    <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">{bot.username}</span>
   </NavBrand>
   <div class="flex items-center md:order-2">
     <Avatar id="avatar-menu" src="https://cdn.discordapp.com/avatars/274973338676494347/00dcf84af54a0a58d2394b4054e0f7f5.png?size=1024&format=webp&quality=lossless&width=0&height=256" />
--- apps/dashboard/src/routes/+layout.svelte
+++ apps/dashboard/src/routes/+layout.svelte
@@ -4,9 +4,11 @@
     import Sidebar from "$lib/components/nav/sidebar.svelte"
     import Navbar from '$lib/components/nav/navbar.svelte';
     export let data: LayoutData;
+    import { type APIUser } from 'discord-api-types/v10'
+    import { page } from "$app/stores"
 </script>
 
-<Navbar>
+<Navbar bot={$page.data.bot}>
     <slot name="navbar"></slot> 
 </Navbar>
 
