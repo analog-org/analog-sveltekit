d65ea7480ec7a9952e5136ef859cda51d6535811
BossDaily <gboys2008@gmail.com>
2024-01-02 23:09:38 -0600

    bot info is in page data regardless of session status

--- apps/dashboard/src/routes/+layout.server.ts
+++ apps/dashboard/src/routes/+layout.server.ts
@@ -1,13 +1,16 @@
-import { REST } from '@discordjs/rest';
+import { REST } from "@discordjs/rest";
 import type { LayoutServerLoad } from "./$types";
-import { Routes, type APIUser } from 'discord-api-types/v10'
-import { DISCORD_CLIENT_ID, DISCORD_CLIENT_SECRET, DISCORD_BOT_TOKEN} from "$env/static/private";
-
+import { Routes, type APIUser } from "discord-api-types/v10";
+import {
+  DISCORD_CLIENT_ID,
+  DISCORD_CLIENT_SECRET,
+  DISCORD_BOT_TOKEN,
+} from "$env/static/private";
 
 export const load: LayoutServerLoad = async (event) => {
   const currentSession = await event.locals.getSession();
-  const botRest = new REST({authPrefix: 'Bot'}).setToken(DISCORD_BOT_TOKEN);
-  const botInfo = await botRest.get(Routes.user('@me'));
+  const botRest = new REST({ authPrefix: "Bot" }).setToken(DISCORD_BOT_TOKEN);
+  const botInfo = await botRest.get(Routes.user("@me"));
 
   if (currentSession) {
     //@ts-ignore
@@ -15,20 +18,19 @@ export const load: LayoutServerLoad = async (event) => {
     //@ts-ignore
     const discordUser: APIUser = currentSession.discordUser;
 
-    const userRest = new REST({authPrefix: 'Bearer'}).setToken(accessToken);
+    const userRest = new REST({ authPrefix: "Bearer" }).setToken(accessToken);
 
-    const userInfo = await userRest.get(Routes.user('@me'));
-    
-    
+    const userInfo = await userRest.get(Routes.user("@me"));
 
     return {
       session: currentSession,
       user: userInfo,
-      
-    }
+      bot: botInfo,
+    };
   }
 
   return {
-    session: currentSession,bot: botInfo,
-  }
-}
+    session: currentSession,
+    bot: botInfo,
+  };
+};
