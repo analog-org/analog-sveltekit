592b87163679d22c5ebc6d2bc2eac64ce9ef6fc7
BossDaily <gboys2008@gmail.com>
2024-01-05 12:42:45 -0600

    Refactor navbar component and add bot avatar and accent color

--- apps/dashboard/src/lib/components/nav/navbar.svelte
+++ apps/dashboard/src/lib/components/nav/navbar.svelte
@@ -11,8 +11,7 @@
     DropdownDivider,
   } from "flowbite-svelte";
   import { type APIUser } from "discord-api-types/v10";
-  /* export let user: APIUser; */
-  export let bot: APIUser;
+
   import * as DropdownMenu from "$lib/components/ui/dropdown-menu";
   import { Button } from "../ui/button";
   import * as Avatar from "$lib/components/ui/avatar";
@@ -21,6 +20,9 @@
 
   import { toggleMode } from "mode-watcher";
   import { signOut, signIn } from "@auth/sveltekit/client";
+  
+  const bot = $page.data.bot as APIUser;
+  const botAccent = $page.data.botColor as string;
 </script>
 
 <Navbar>
@@ -28,18 +30,21 @@
     <img
       src={`https://cdn.discordapp.com/avatars/${bot.id}/${bot.avatar}.png?size=1024&format=webp&quality=lossless&width=0&height=256`}
       class="me-3 h-6 sm:h-14 rounded-full"
+      style="box-shadow: 0 0 20px {botAccent}, 0 0 5px {botAccent};"
       alt={`${bot.username} Logo`}
     />
+
     <span
-      class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
-      >{bot.username}</span
-    >
+      class="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+      >{bot.username}
+    </span>
   </NavBrand>
 
   <NavUl>
     <NavLi href="/" active={true}>Home</NavLi>
     <NavLi href="/dashboard" active={true}>Dashboard</NavLi>
   </NavUl>
+
   <Button on:click={toggleMode} variant="outline" size="icon">
     <Sun
       class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
@@ -53,14 +58,16 @@
   {#if $page.data.session}
     <DropdownMenu.Root>
       <DropdownMenu.Trigger>
-        <Avatar.Root>
-          <Avatar.Image
-            src={`https://cdn.discordapp.com/avatars/${$page.data.user.id}/${$page.data.user.avatar}.png?size=1024&format=webp&quality=lossless&width=0&height=256`}
-            alt="@shadcn"
-          />
-          <Avatar.Fallback>CN</Avatar.Fallback>
-        </Avatar.Root>
-        <NavHamburger class1="w-full md:flex md:w-auto md:order-1" />
+        <div class="flex items-center md:order-2">
+          <Avatar.Root>
+            <Avatar.Image
+              src={`https://cdn.discordapp.com/avatars/${$page.data.user.id}/${$page.data.user.avatar}.png?size=1024&format=webp&quality=lossless&width=0&height=256`}
+              alt="@shadcn"
+            />
+            <Avatar.Fallback>CN</Avatar.Fallback>
+          </Avatar.Root>
+          <NavHamburger class1="w-full md:flex md:w-auto md:order-1" />
+        </div>
       </DropdownMenu.Trigger>
       <DropdownMenu.Content class="w-56">
         <DropdownMenu.Item on:click={signOut}>Sign out</DropdownMenu.Item>
