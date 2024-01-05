59f049c3871b2e30b4e31742fe6172a309453ada
BossDaily <gboys2008@gmail.com>
2024-01-05 12:43:05 -0600

    Add bot accent color to layout server load

--- apps/dashboard/src/routes/+layout.server.ts
+++ apps/dashboard/src/routes/+layout.server.ts
@@ -1,6 +1,7 @@
 import { REST } from "@discordjs/rest";
 import type { LayoutServerLoad } from "./$types";
-import { Routes, type APIUser } from "discord-api-types/v10";
+import { Routes, type APIUser, type APIGuild } from "discord-api-types/v10";
+import Vibrant from "node-vibrant";
 import {
   DISCORD_CLIENT_ID,
   DISCORD_CLIENT_SECRET,
@@ -10,7 +11,12 @@ import {
 export const load: LayoutServerLoad = async (event) => {
   const currentSession = await event.locals.getSession();
   const botRest = new REST({ authPrefix: "Bot" }).setToken(DISCORD_BOT_TOKEN);
-  const botInfo = await botRest.get(Routes.user("@me"));
+  const botInfo = await botRest.get(Routes.user("@me")).then((res) => res as APIUser);
+  const botGuilds = await botRest.get(Routes.userGuilds()).then((res) => res as APIGuild[]);
+
+  const botAccent = await Vibrant.from(`https://cdn.discordapp.com/avatars/${botInfo?.id}/${botInfo?.avatar}.png?size=1024&format=webp&quality=lossless&width=0&height=256`).getPalette()
+  .then((palette) => palette.Vibrant?.hex)
+
 
   if (currentSession) {
     //@ts-ignore
@@ -20,17 +26,20 @@ export const load: LayoutServerLoad = async (event) => {
 
     const userRest = new REST({ authPrefix: "Bearer" }).setToken(accessToken);
 
-    const userInfo = await userRest.get(Routes.user("@me"));
+    const userInfo = await userRest.get(Routes.user("@me")).then((res) => res as APIUser);
+    const userGuilds  = await userRest.get(Routes.userGuilds()).then((res) => res as APIGuild[]);
 
     return {
       session: currentSession,
       user: userInfo,
       bot: botInfo,
+      botColor: botAccent,
     };
   }
 
   return {
     session: currentSession,
     bot: botInfo,
+    botColor: botAccent,
   };
 };
