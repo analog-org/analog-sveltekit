dc8f121223c867a524f766e2b06c491da32853f3
BossDaily <gboys2008@gmail.com>
2024-01-05 13:18:20 -0600

    Loads user guilds, and mutualGuilds to the page

--- apps/dashboard/src/routes/+layout.server.ts
+++ apps/dashboard/src/routes/+layout.server.ts
@@ -7,16 +7,24 @@ import {
   DISCORD_CLIENT_SECRET,
   DISCORD_BOT_TOKEN,
 } from "$env/static/private";
+import { PermissionFlagsBits } from "discord-api-types/v10";
+import { BitField, enumToObject } from "@sapphire/bitfield";
 
 export const load: LayoutServerLoad = async (event) => {
   const currentSession = await event.locals.getSession();
   const botRest = new REST({ authPrefix: "Bot" }).setToken(DISCORD_BOT_TOKEN);
-  const botInfo = await botRest.get(Routes.user("@me")).then((res) => res as APIUser);
-  const botGuilds = await botRest.get(Routes.userGuilds()).then((res) => res as APIGuild[]);
+  const botInfo = await botRest
+    .get(Routes.user("@me"))
+    .then((res) => res as APIUser);
+  const botGuilds = await botRest
+    .get(Routes.userGuilds())
+    .then((res) => res as APIGuild[]);
 
-  const botAccent = await Vibrant.from(`https://cdn.discordapp.com/avatars/${botInfo?.id}/${botInfo?.avatar}.png?size=1024&format=webp&quality=lossless&width=0&height=256`).getPalette()
-  .then((palette) => palette.Vibrant?.hex)
-
+  const botAccent = await Vibrant.from(
+    `https://cdn.discordapp.com/avatars/${botInfo?.id}/${botInfo?.avatar}.png?size=1024&format=webp&quality=lossless&width=0&height=256`
+  )
+    .getPalette()
+    .then((palette) => palette.Vibrant?.hex);
 
   if (currentSession) {
     //@ts-ignore
@@ -26,14 +34,36 @@ export const load: LayoutServerLoad = async (event) => {
 
     const userRest = new REST({ authPrefix: "Bearer" }).setToken(accessToken);
 
-    const userInfo = await userRest.get(Routes.user("@me")).then((res) => res as APIUser);
-    const userGuilds  = await userRest.get(Routes.userGuilds()).then((res) => res as APIGuild[]);
+    const userInfo = await userRest
+      .get(Routes.user("@me"))
+      .then((res) => res as APIUser);
+    const userGuilds = await userRest
+      .get(Routes.userGuilds())
+      .then((res) => res as APIGuild[]);
+    const adminGuilds = userGuilds
+      .map((guild) => {
+        const p: bigint = BigInt(!guild?.permissions);
+        const PermissionsBitField = new BitField(
+          enumToObject(PermissionFlagsBits)
+        );
+        const hasManageGuild =
+          PermissionsBitField.has(p, PermissionFlagsBits.Administrator) ||
+          PermissionsBitField.has(p, PermissionFlagsBits.ManageGuild);
+        if (guild.owner || hasManageGuild) return guild;
+      })
+    
+    const mutualGuilds = adminGuilds.filter((guild) => {
+      return botGuilds.some((botGuild) => botGuild.id === guild?.id);
+    });
 
     return {
       session: currentSession,
       user: userInfo,
       bot: botInfo,
       botColor: botAccent,
+      botGuilds: botGuilds,
+      userGuilds: adminGuilds,
+      mutualGuilds: mutualGuilds,
     };
   }
 
@@ -41,5 +71,6 @@ export const load: LayoutServerLoad = async (event) => {
     session: currentSession,
     bot: botInfo,
     botColor: botAccent,
+    botGuilds: botGuilds,
   };
 };
