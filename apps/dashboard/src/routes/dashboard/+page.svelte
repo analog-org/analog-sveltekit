65600f7432c3dadb92e5bbd0e882b46101cb8a54
BossDaily <gboys2008@gmail.com>
2024-02-08 18:39:26 -0600

    Added dashboard page

---
+++ apps/dashboard/src/routes/dashboard/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import type { PageData } from "./$types";
+
+  export let data: PageData;
+  console.log(data.mutualGuilds);
+</script>
+
+<div>
+  <p>tha dashboard</p>
+  {#each data?.mutualGuilds || [] as guild (guild?.id)}
+    <p>{guild?.name}</p>
+  {/each}
+</div>
