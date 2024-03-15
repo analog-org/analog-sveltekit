5f0c33dda5e547d9d80d1a4bdd9ff56d6de2fd65
BossDaily <gboys2008@gmail.com>
2024-03-15 16:32:43 -0500

    Basic selection ui

--- apps/dashboard/src/routes/dashboard/+page.svelte
+++ apps/dashboard/src/routes/dashboard/+page.svelte
@@ -1,13 +1,20 @@
 <script lang="ts">
   import type { PageData } from "./$types";
+  import * as Avatar from "$lib/components/ui/avatar";
 
   export let data: PageData;
-  console.log(data.mutualGuilds);
+  console.log(data.userGuilds);
 </script>
 
-<div>
-  <p>tha dashboard</p>
-  {#each data?.mutualGuilds || [] as guild (guild?.id)}
-    <p>{guild?.name}</p>
+<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4 justify-items-center">
+  {#each data?.userGuilds || [] as guild (guild?.id)}
+    <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6">
+      <Avatar.Root>
+        <Avatar.Image
+          src={`https://cdn.discordapp.com/icons/${guild?.id}/${guild?.icon}.png`}
+        />
+        <Avatar.Fallback>{guild?.name[0]}{guild?.name[1]}</Avatar.Fallback>
+      </Avatar.Root>
+    </div>
   {/each}
 </div>
