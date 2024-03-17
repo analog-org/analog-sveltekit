d4d96010712aa660d79580aef45f4353d0e02b3c
BossDaily <gboys2008@gmail.com>
2024-03-17 16:50:55 -0500

    (Dashboard) removed commented out components and guilds list logged to console

--- apps/dashboard/src/routes/dashboard/+page.svelte
+++ apps/dashboard/src/routes/dashboard/+page.svelte
@@ -4,38 +4,7 @@
   import * as Avatar from "$lib/components/ui/avatar";
 
   export let data: PageData;
-  console.log(data.userGuilds);
 </script>
-
-<!-- <div
-  class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4 justify-items-center"
->
-  {#each data?.mutualGuilds || [] as guild (guild?.id)}
-    <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6">
-      <a href={`/dashboard/${guild?.id}`}>
-        <Avatar.Root>
-          <Avatar.Image
-            src={`https://cdn.discordapp.com/icons/${guild?.id}/${guild?.icon}.png`}
-          />
-          <Avatar.Fallback>{guild?.name[0]}{guild?.name[1]}</Avatar.Fallback>
-        </Avatar.Root>
-      </a>
-    </div>
-  {/each}
-  {#each data?.joinAbleGuilds || [] as guild (guild?.id)}
-    <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6">
-      <a href={`https://discord.com/oauth2/authorize?client_id=${data?.bot.id}&permissions=8&scope=bot+applications.commands&guild_id=${guild.id}&disable_guild_select=true`}>
-        <Avatar.Root>
-          <Avatar.Image
-            src={`https://cdn.discordapp.com/icons/${guild?.id}/${guild?.icon}.png`}
-          />
-          <Avatar.Fallback>{guild?.name[0]}{guild?.name[1]}</Avatar.Fallback>
-        </Avatar.Root>
-      </a>
-    </div>
-  {/each}
-</div>
- -->
 
 <div
   class=" px-4 py-8 mx-auto sm:px-6 lg:px-8 rounded-xl flex flex-col sm:flex-row flex-initial flex-wrap justify-center"
