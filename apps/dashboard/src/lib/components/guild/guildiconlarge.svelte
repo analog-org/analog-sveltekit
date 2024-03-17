47d75df12a0af62653d4003e83d8c3ffe2315cc9
BossDaily <gboys2008@gmail.com>
2024-03-17 15:26:28 -0500

    (Dashboard) Large guild icon component

---
+++ apps/dashboard/src/lib/components/guild/guildiconlarge.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  export let guildId: string;
+  export let guildIcon: string | null;
+  export let userDiscriminator: number;
+</script>
+
+{#if guildIcon}
+  <img 
+    src={`https://cdn.discordapp.com/icons/${guildId}/${guildIcon}.png`} 
+    class="blur-md rounded-xl" 
+    alt="Guild Icon" 
+    style="object-fit: cover; width: 100%; height: 100%;" 
+  />
+{:else}
+  <img 
+    src={`https://cdn.discordapp.com/embed/avatars/${userDiscriminator % 5}.png`} 
+    class="blur-md" 
+    alt="Default Guild Icon" 
+    style="object-fit: cover; width: 100%; height: 100%;" 
+  />
+{/if}
