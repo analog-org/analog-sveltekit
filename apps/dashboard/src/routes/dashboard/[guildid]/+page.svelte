73f4cd574cb34f09401b0b99bb5620acbfa4b2c9
BossDaily <gboys2008@gmail.com>
2024-03-18 20:15:40 -0500

    (Dashboard) Basic card component with basic guild information

--- apps/dashboard/src/routes/dashboard/[guildid]/+page.svelte
+++ apps/dashboard/src/routes/dashboard/[guildid]/+page.svelte
@@ -1,6 +1,29 @@
 <script lang="ts">
-    import type { PageData } from './$types';
-    
-    export let data: PageData;
+  import type { PageData } from "./$types";
+  import * as Card from "$lib/components/ui/card";
+
+  export let data: PageData;
+  const guild = data?.guild;
+  console.log(guild)
 </script>
 
+<div class="flex justify-center items-center min-h-screen">
+  <Card.Root>
+    <Card.Header>
+        <img 
+        src={`https://cdn.discordapp.com/icons/${guild?.id}/${guild?.icon}.png`} 
+        width="96" 
+        height="96" 
+        class="rounded-full" 
+        alt="Guild Icon" 
+      />
+    </Card.Header>
+    <Card.Content>
+      <p>Guild Name: {guild?.name}</p>
+      <p> Guild ID: {guild?.id}</p>
+    </Card.Content>
+    <Card.Footer>
+      <p>{guild?.features}</p>
+    </Card.Footer>
+  </Card.Root>
+</div>
