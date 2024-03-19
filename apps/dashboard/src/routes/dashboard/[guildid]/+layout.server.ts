22634362fa0dc5ff3f0933d9a7142ddaddb246c7
BossDaily <gboys2008@gmail.com>
2024-03-18 20:13:22 -0500

    returns guild information in page data

--- apps/dashboard/src/routes/dashboard/[guildid]/+layout.server.ts
+++ apps/dashboard/src/routes/dashboard/[guildid]/+layout.server.ts
@@ -1,13 +1,18 @@
-import type { LayoutServerLoad } from './$types';
+import type { LayoutServerLoad } from "./$types";
 import { page } from "$app/stores";
-import { redirect } from '@sveltejs/kit';
+import { redirect } from "@sveltejs/kit";
 
-import type { APIGuild } from 'discord-api-types/v10';
+import type { APIGuild } from "discord-api-types/v10";
 
 export const load = (async ({ parent, params }) => {
-    const { mutualGuilds } = await parent()
-    const guildId = params.guildid;
-    if (!mutualGuilds?.some((guild: APIGuild) => guild.id === guildId)) throw redirect(303, '/auth/signin')
-    
-    return {};
+  const { mutualGuilds } = await parent();
+  const guildId = params.guildid;
+  if (!mutualGuilds?.some((guild: APIGuild) => guild.id === guildId))
+    throw redirect(303, "/dashboard");
+
+  const guild = mutualGuilds.find((guild: APIGuild) => guild.id === guildId);  
+
+  return {
+    guild,
+  };
 }) satisfies LayoutServerLoad;
