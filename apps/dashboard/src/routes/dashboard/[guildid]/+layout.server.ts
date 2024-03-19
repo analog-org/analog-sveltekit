2442b49a81e2ba4ef357b1b5b235c289a247a2a9
BossDaily <gboys2008@gmail.com>
2024-03-18 19:59:12 -0500

    (Dashboard) Redirects user to signin if they do not have proper permissions in the guild

--- apps/dashboard/src/routes/dashboard/[guildid]/+layout.server.ts
+++ apps/dashboard/src/routes/dashboard/[guildid]/+layout.server.ts
@@ -1,6 +1,13 @@
 import type { LayoutServerLoad } from './$types';
+import { page } from "$app/stores";
+import { redirect } from '@sveltejs/kit';
 
-export const load = (async () => {
+import type { APIGuild } from 'discord-api-types/v10';
+
+export const load = (async ({ parent, params }) => {
+    const { mutualGuilds } = await parent()
+    const guildId = params.guildid;
+    if (!mutualGuilds?.some((guild: APIGuild) => guild.id === guildId)) throw redirect(303, '/auth/signin')
     
     return {};
 }) satisfies LayoutServerLoad;
