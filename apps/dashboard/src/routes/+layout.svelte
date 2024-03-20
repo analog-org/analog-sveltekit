ac58342b8f777b2d7fcea5ac0a42ab82b06f3597
BossDaily <gboys2008@gmail.com>
2024-03-20 12:53:02 -0500

    (Dashboard) Dynamic favicon that uses the discord bot pfp

--- apps/dashboard/src/routes/+layout.svelte
+++ apps/dashboard/src/routes/+layout.svelte
@@ -7,10 +7,14 @@
     import { type APIUser } from 'discord-api-types/v10'
     import { page } from "$app/stores"
     import { ModeWatcher } from "mode-watcher";
+    const bot = $page.data.bot as APIUser;
 </script>
+<svelte:head>
+    <link rel="icon" type="image/png" href={`https://cdn.discordapp.com/avatars/${bot.id}/${bot.avatar}.png?size=1024&format=webp&quality=lossless&width=0&height=256`} class="" />
+</svelte:head>
 
 <ModeWatcher />
-<Navbar bot={$page.data.bot}>
+<Navbar bot={bot}>
     <slot name="navbar"></slot> 
 </Navbar>
 
