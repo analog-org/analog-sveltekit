a93324df02dc98e42b2599783f1457f4aecca34b
BossDaily <gboys2008@gmail.com>
2024-03-20 19:47:50 -0500

    (Dashboard) new route system

---
+++ apps/dashboard/src/routes/(landing)/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import type { LayoutData } from './$types';
+    import Navbar from '$lib/components/nav/navbar.svelte';
+    export let data: LayoutData;
+</script>
+
+<Navbar />
+
+<slot></slot>
