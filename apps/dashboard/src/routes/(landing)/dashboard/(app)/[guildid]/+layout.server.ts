5f6d0f3227ebb3693bd3a063901cf5a52c544e30
BossDaily <gboys2008@gmail.com>
2024-03-29 17:20:50 -0500

    (Dashboard) Properly Redirects user to dashboard if they do not have the proper permissions for the guild

--- apps/dashboard/src/routes/(landing)/dashboard/(app)/[guildid]/+layout.server.ts
+++ apps/dashboard/src/routes/(landing)/dashboard/(app)/[guildid]/+layout.server.ts
@@ -1,19 +1,27 @@
 import type { LayoutServerLoad } from "./$types";
 import { page } from "$app/stores";
 import { redirect } from "@sveltejs/kit";
+import { REST } from "@discordjs/rest";
+import { Routes, type APIUser, type APIGuild } from "discord-api-types/v10";
+import Vibrant from "node-vibrant";
+import {
+  DISCORD_CLIENT_ID,
+  DISCORD_CLIENT_SECRET,
+  DISCORD_TOKEN,
+} from "$env/static/private";
+import { PermissionFlagsBits } from "discord-api-types/v10";
+import { BitField, enumToObject } from "@sapphire/bitfield";
+import { loadUserInfo } from "$lib/utils";
 
-import type { APIGuild } from "discord-api-types/v10";
 
-export const load = (async ({ parent, params, locals }) => {
-  const parentData = await parent();
-  console.log(parentData)
-  const guildId = params.guildid;/* 
-  if (!mutualGuilds?.some((guild: APIGuild) => guild.id === guildId))
-    throw redirect(303, "/dashboard"); 
+export const load: LayoutServerLoad = async ({ locals, params }) => {
+  const guildId = params.guildid;
+  const userInfo = await loadUserInfo({ locals }, DISCORD_TOKEN)
+  const mutualGuilds = userInfo?.mutualGuilds;
+   if (!mutualGuilds?.some((guild: APIGuild) => guild.id === guildId))
+    throw redirect(303, "/dashboard");  
 
-  const guild = mutualGuilds.find((guild: APIGuild) => guild.id === guildId);  */
-  
-  return {
-    /* guild, */
-  };
-}) satisfies LayoutServerLoad;
+  console.log(mutualGuilds)
+  const guild = mutualGuilds?.find((guild: APIGuild) => guild.id === guildId); 
+  return userInfo;
+};
