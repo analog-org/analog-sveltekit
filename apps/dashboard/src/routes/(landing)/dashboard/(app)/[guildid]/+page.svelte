e2b95cf3bdb42da506289ecb5787566c469fe368
BossDaily <gboys2008@gmail.com>
2024-03-29 18:44:30 -0500

    (Dashboard) Uncommented Guild Card

--- apps/dashboard/src/routes/(landing)/dashboard/(app)/[guildid]/+page.svelte
+++ apps/dashboard/src/routes/(landing)/dashboard/(app)/[guildid]/+page.svelte
@@ -7,23 +7,22 @@
 </script>
 
 <div class="flex justify-center items-center min-h-screen">
-  hi
-<Card.Root>
+  <Card.Root>
     <Card.Header>
-        <img 
-        src={`https://cdn.discordapp.com/icons/${guild?.id}/${guild?.icon}.png`} 
-        width="96" 
-        height="96" 
-        class="rounded-full" 
-        alt="Guild Icon" 
+      <img
+        src={`https://cdn.discordapp.com/icons/${guild?.id}/${guild?.icon}.png`}
+        width="96"
+        height="96"
+        class="rounded-full"
+        alt="Guild Icon"
       />
     </Card.Header>
     <Card.Content>
       <p>Guild Name: {guild?.name}</p>
-      <p> Guild ID: {guild?.id}</p>
+      <p>Guild ID: {guild?.id}</p>
     </Card.Content>
     <Card.Footer>
       <p>{guild?.features}</p>
     </Card.Footer>
-  </Card.Root>  
+  </Card.Root>
 </div>
