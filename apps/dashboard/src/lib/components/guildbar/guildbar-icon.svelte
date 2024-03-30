e8ad3d908d19ec72c09c3face2e02135de740795
BossDaily <gboys2008@gmail.com>
2024-03-30 15:43:04 -0500

    (Dashboard) GuildBar icon component

---
+++ apps/dashboard/src/lib/components/guildbar/guildbar-icon.svelte
@@ -0,0 +1,29 @@
+<script  async script lang="ts">
+  import { type APIGuild } from "discord-api-types/v10";
+  import { page } from "$app/stores";
+  import Vibrant from "node-vibrant";
+  const data = $page.data;
+  const guild = data.guild as APIGuild;
+
+  const guildIcon = `https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}.png`;
+  
+  const guildAccent = await Vibrant.from(guildIcon)
+    .getPalette()
+    .then((palette) => palette.Vibrant?.hex);
+
+  console.log(guildAccent);
+</script>
+
+<div
+  class="flex justify-center items-center flex-grow-0 flex-shrink-0 relative overflow-hidden gap-2.5 px-2.5 rounded-xl"
+>
+  <img
+    src={guildIcon}
+    class="flex-grow-0 flex-shrink-0 w-12 h-12 rounded-[70px] object-cover"
+    style="box-shadow: 0 0 20px {guildAccent}, 0 0 5px {guildAccent};"
+    alt={guild.name}
+  />
+  <p class="flex-grow-0 flex-shrink-0 text-2xl font-bold text-left text-white">
+    {guild.name}
+  </p>
+</div>
