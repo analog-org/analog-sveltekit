0864279454b448d05327b76cc89d2d66931257f3
BossDaily <gboys2008@gmail.com>
2024-03-30 15:09:22 -0500

    (Dashboard) Guild Bar

---
+++ apps/dashboard/src/lib/components/guildbar/guildbar.svelte
@@ -0,0 +1,5 @@
+<div
+  class="flex justify-between items-center self-stretch flex-grow-0 flex-shrink-0 h-[90px] overflow-hidden rounded-[20px] bg-white/[0.01] border border-white/[0.01] backdrop-blur-[6px]"
+>
+  <slot />
+</div>
