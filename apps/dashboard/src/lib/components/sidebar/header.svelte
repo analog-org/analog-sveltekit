a4c6793b2b32f625495a4ec5271fdfd52773a11d
BossDaily <gboys2008@gmail.com>
2024-04-03 16:39:00 -0500

    (Dashboard) Header component for sidebar

---
+++ apps/dashboard/src/lib/components/sidebar/header.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import type { APIUser } from "discord-api-types/v10";
+  export let bot: APIUser;
+</script>
+
+<div class="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
+  <a href="/" class="flex items-center gap-2 font-semibold">
+    <img
+      class="h-6 w-6 rounded-full"
+      src={`https://cdn.discordapp.com/avatars/${bot?.id}/${bot?.avatar}.png?size=1024&format=webp&quality=lossless&width=0&height=256`}
+      alt={bot?.username}
+    />
+    <span class="">{bot.username}</span>
+  </a>
+</div>
