83cfbade6e34cbf81561723829d9d3cd5a94bce4
BossDaily <gboys2008@gmail.com>
2024-04-03 16:53:47 -0500

    (Dashboard) Added types and dynamic icon

---
+++ apps/dashboard/src/lib/components/sidebar/sidebar-item.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  export let href: string;
+  export let icon: string;
+  export let label: string;
+</script>
+
+<a
+  href={href}
+  class="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+>
+  <img src={`https://unpkg.com/lucide-static@latest/icons/${icon}.svg`} class="h-4 w-4" alt={label}/>
+  {label}
+</a>
