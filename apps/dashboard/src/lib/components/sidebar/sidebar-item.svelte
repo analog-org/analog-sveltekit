09b65f1afd568d0a8fb1ef2bd1d6874d645739e6
BossDaily <gboys2008@gmail.com>
2024-04-04 15:41:26 -0500

    (Dashboard) Added background for icon

--- apps/dashboard/src/lib/components/sidebar/sidebar-item.svelte
+++ apps/dashboard/src/lib/components/sidebar/sidebar-item.svelte
@@ -5,12 +5,19 @@
 </script>
 
 <a
-  href={href}
+  {href}
   class="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
 >
-<svg class="h-6 w-6">
-  <image href={`https://unpkg.com/lucide-static@latest/icons/${icon}.svg`} class="h-6 w-6 brightness-0 invert"/>
-</svg>
-  
+  <div
+    class="flex justify-center items-center flex-grow-0 flex-shrink-0 w-6 h-6 relative overflow-hidden gap-2.5 px-2 py-[7px] rounded-[5px] bg-zinc-600 border-2 border-zinc-500"
+    style="box-shadow: 0px 0px 7px 0 rgba(113,113,122,0.7);"
+  >
+    <svg class="h-4 w-4 flex-grow-0 flex-shrink-0">
+      <image
+        href={`https://unpkg.com/lucide-static@latest/icons/${icon}.svg`}
+        class="h-4 w-4 brightness-0 invert"
+      />
+    </svg>
+  </div>
   {label}
 </a>
