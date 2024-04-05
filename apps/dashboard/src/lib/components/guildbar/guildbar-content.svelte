41698b3578eb42ba957d3a50c210b1a2956588f1
BossDaily <gboys2008@gmail.com>
2024-04-04 22:20:26 -0500

    (Dashboard) Matches new color scheme

--- apps/dashboard/src/lib/components/guildbar/guildbar-content.svelte
+++ apps/dashboard/src/lib/components/guildbar/guildbar-content.svelte
@@ -1,5 +1,5 @@
 <div
-  class="flex justify-center items-center self-stretch flex-grow-0 flex-shrink-0 relative overflow-hidden gap-2.5 px-2.5 rounded-[20px] dark:bg-neutral-900 bg-white"
+  class="flex justify-center items-center self-stretch flex-grow-0 flex-shrink-0 relative overflow-hidden gap-2.5 px-2.5 rounded-[20px] dark:bg-muted/40 bg-white"
 >
   <slot />
 </div>
