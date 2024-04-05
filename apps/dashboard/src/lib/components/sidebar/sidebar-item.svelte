af665a50e067ea44fd892d16cfaf6eedde4d8679
BossDaily <gboys2008@gmail.com>
2024-04-04 23:05:53 -0500

    (Dashboard) Better dark theme support and supports when item is being hovered

--- apps/dashboard/src/lib/components/sidebar/sidebar-item.svelte
+++ apps/dashboard/src/lib/components/sidebar/sidebar-item.svelte
@@ -9,7 +9,7 @@
 {#if active}
 <a
   {href}
-  class="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary bg-zinc-800"
+  class="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary dark:bg-zinc-800 bg-zinc-200"
 >
   <div
   class="flex justify-center items-center flex-grow-0 flex-shrink-0 w-6 h-6 relative overflow-hidden gap-2.5 px-2 py-[7px] rounded-[5px] bg-[#4648e5] border-2 border-[#5765f0]"
@@ -22,7 +22,7 @@
       />
     </svg>
   </div>
-  <p class="text-white">
+  <p class="dark:text-white text-black">
     {label}
   </p>
   
@@ -33,7 +33,7 @@
   class=" flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
 >
   <div
-    class="flex justify-center items-center flex-grow-0 flex-shrink-0 w-6 h-6 relative overflow-hidden gap-2.5 px-2 py-[7px] rounded-[5px] bg-zinc-600 border-2 border-zinc-500"
+    class="flex justify-center items-center flex-grow-0 flex-shrink-0 w-6 h-6 relative overflow-hidden gap-2.5 px-2 py-[7px] rounded-[5px] bg-zinc-600 border-2 border-zinc-500 hover:dark:bg-zinc-800 hover:bg-zinc-200"
     style="box-shadow: 0px 0px 7px 0 rgba(113,113,122,0.7);"
   >
     <svg class="h-4 w-4 flex-grow-0 flex-shrink-0">
