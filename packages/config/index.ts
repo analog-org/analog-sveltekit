d5d470121ee1b4a1266ac095a15e26809781678f
BossDaily <gboys2008@gmail.com>
2024-04-04 23:01:59 -0500

    (Dashboard) Added disclaimer about default path

--- packages/config/index.ts
+++ packages/config/index.ts
@@ -3,7 +3,8 @@ export const config = {
     category: [
       // icons list: https://lucide.dev/icons
       // the name has to match the page path
-      { name: "General", pages: [{ name: "home", icon: "home" }] },
+      // everything in the default path will not have a dropdown menu
+      { name: "Default", pages: [{ name: "home", icon: "home" }] },
       { name: "Moderation", pages: [{ name: "embed", icon: "layers" }] },
     ],
   },
