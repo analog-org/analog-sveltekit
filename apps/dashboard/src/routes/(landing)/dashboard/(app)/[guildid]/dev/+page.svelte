3991e664bba5531a70370776537e7a634315c24a
BossDaily <gboys2008@gmail.com>
2024-04-13 22:47:34 -0500

    (Dashboard) Roles test

--- apps/dashboard/src/routes/(landing)/dashboard/(app)/[guildid]/dev/+page.svelte
+++ apps/dashboard/src/routes/(landing)/dashboard/(app)/[guildid]/dev/+page.svelte
@@ -1,24 +1,32 @@
 <script lang="ts">
-    import type { PageData } from './$types';
-    import ChannelInput from '$lib/components/inputs/channel-input.svelte';
-	import ColorPicker from 'svelte-awesome-color-picker';
-    import { writable } from 'svelte/store';
-  import type { APIChannel } from 'discord-api-types/v10';
-  import { onMount } from 'svelte';
-    let hex: string
-    let selectedChannel = writable<APIChannel | undefined>(undefined);
+  import type { PageData } from "./$types";
+  import ChannelInput from "$lib/components/inputs/channel-input.svelte";
+  import ColorPicker from "svelte-awesome-color-picker";
+  import { writable } from "svelte/store";
+  import type { APIChannel, APIRole } from "discord-api-types/v10";
+
+  import RoleInput from "$lib/components/inputs/role-input.svelte";
+
+  let hex: string;
+  let selectedChannel = writable<APIChannel | undefined>(undefined);
+  let selectedRole = writable<APIRole | undefined>(undefined);
+
 
 
     
-    export let data: PageData;
+   
+  export let data: PageData;
 </script>
 
 <center>
-    Hello this is a test
+  Hello this is a test
 
-    <ChannelInput bind:selectedChannel channels={data?.channels} />
-    <p>Selected channel: {$selectedChannel ? $selectedChannel.name : 'None'}</p>
+  <ChannelInput bind:selectedChannel channels={data?.channels} />
+  <p>Selected channel: {$selectedChannel ? $selectedChannel.name : "None"}</p>
 
-    <ColorPicker bind:hex  /> <p style={`color: ${hex}`}>selected color: {hex}</p>
+  <RoleInput bind:selectedRole roles={data?.roles} />
+    <p>Selected role: {$selectedRole ? $selectedRole.name : "None"} {$selectedRole?.color.toString(16).padStart(6, '0')}</p>
+
+  <ColorPicker bind:hex />
+  <p style={`color: ${hex}`}>selected color: {hex}</p>
 </center>
-
