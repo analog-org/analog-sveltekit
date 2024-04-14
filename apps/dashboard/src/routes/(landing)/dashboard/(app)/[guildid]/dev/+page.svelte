bfb766889ed78269844934aa60e86e6b86065c52
BossDaily <gboys2008@gmail.com>
2024-04-13 21:10:52 -0500

    (Dashboard) testing what is being selected

--- apps/dashboard/src/routes/(landing)/dashboard/(app)/[guildid]/dev/+page.svelte
+++ apps/dashboard/src/routes/(landing)/dashboard/(app)/[guildid]/dev/+page.svelte
@@ -2,7 +2,13 @@
     import type { PageData } from './$types';
     import ChannelInput from '$lib/components/inputs/channel-input.svelte';
 	import ColorPicker from 'svelte-awesome-color-picker';
+    import { writable } from 'svelte/store';
+  import type { APIChannel } from 'discord-api-types/v10';
+  import { onMount } from 'svelte';
     let hex: string
+    let selectedChannel = writable<APIChannel | undefined>(undefined);
+
+
     
     export let data: PageData;
 </script>
@@ -10,7 +16,9 @@
 <center>
     Hello this is a test
 
-    <ChannelInput channels={data?.channels} />
-    <ColorPicker bind:hex  />
+    <ChannelInput bind:selectedChannel channels={data?.channels} />
+    <p>Selected channel: {$selectedChannel ? $selectedChannel.name : 'None'}</p>
+
+    <ColorPicker bind:hex  /> <p style={`color: ${hex}`}>selected color: {hex}</p>
 </center>
 
