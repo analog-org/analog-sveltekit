aa5c2520b1d23d9b32eb321e97d5d814f91f1c3c
BossDaily <gboys2008@gmail.com>
2024-04-16 18:50:09 -0500

    (Dashboard) Removed svelte awesome color picker

--- apps/dashboard/src/routes/(landing)/dashboard/(app)/[guildid]/dev/+page.svelte
+++ apps/dashboard/src/routes/(landing)/dashboard/(app)/[guildid]/dev/+page.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
   import type { PageData } from "./$types";
   import ChannelInput from "$lib/components/inputs/channel-input.svelte";
-  import ColorPicker from "svelte-awesome-color-picker";
+
   import { writable } from "svelte/store";
   import type { APIChannel, APIRole } from "discord-api-types/v10";
 
   import RoleInput from "$lib/components/inputs/role-input.svelte";
 
-  let hex: string;
   let selectedChannel = writable<APIChannel | undefined>(undefined);
   let selectedRole = writable<APIRole | undefined>(undefined);
 
@@ -29,8 +28,10 @@
     Selected role: {$selectedRole ? $selectedRole.name : "None"}
     {$selectedRole?.color.toString(16).padStart(6, "0")}
   </p>
-
-  <ColorInput bind:color  class="w-52 "  /> 
-  <ColorPicker bind:hex  />
-  <p style={`color: ${hex}`}>selected color: {hex}</p>
+  <div class="w-52">
+    <ColorInput bind:color title={color.toHexString()} />
+    <p style={`color: ${color.toHexString()}`}>
+      selected color: {color.toHexString()}
+    </p>
+  </div>
 </center>
