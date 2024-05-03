7c6a267a0359863bd4f784920a7b77b51623c12f
BossDaily <gboys2008@gmail.com>
2024-05-03 17:17:43 -0500

    (Dashboard) Debugging selected role by showing badge

--- apps/dashboard/src/routes/(landing)/dashboard/(app)/[guildid]/dev/+page.svelte
+++ apps/dashboard/src/routes/(landing)/dashboard/(app)/[guildid]/dev/+page.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
   import type { PageData } from "./$types";
   import ChannelInput from "$lib/components/inputs/channel-input.svelte";
+  import { mode } from "mode-watcher";
 
   import { writable } from "svelte/store";
   import type { APIChannel, APIRole } from "discord-api-types/v10";
 
   import OldRoleInput from "$lib/components/inputs/role-input-copy.svelte";
   import RoleInput from "$lib/components/inputs/role-input.svelte";
+
+  import { Badge } from "$lib/components/ui/badge";
 
   let selectedChannel = writable<APIChannel | undefined>(undefined);
   let oldSelectedRole = writable<APIRole | undefined>(undefined);
@@ -16,7 +19,7 @@
 
   let color = new Color("#ff3d91");
 
-   $: console.log(selectedRole)
+  $: console.log(selectedRole);
 
   export let data: PageData;
 </script>
@@ -33,11 +36,18 @@
     (Old) Selected role: {$oldSelectedRole ? $oldSelectedRole.name : "None"}
     {$oldSelectedRole?.color.toString(16).padStart(6, "0")}
   </p>
-
+  
+  {#if selectedRole}
+    {#each selectedRole as role}
+      <Badge
+        class="bg-transparent"
+        style={`background: #${role.color.toString(16).padStart(6, `${$mode === "dark" ? "f" : "0"}`)}`}
+      >
+        {role.name}
+      </Badge>
+    {/each}
+  {/if}
   <RoleInput bind:selectedRoles={selectedRole} roles={data?.roles} />
-  <p>
-    Selected role: {selectedRole ? selectedRole?.map((roles)=> roles?.name).join(", ") : "None"}
-  </p>
 
   <div class="w-52">
     <ColorInput bind:color title={color.toHexString()} />
