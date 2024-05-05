0ecb3d900e71839d93c33a1d30b92f002398f946
BossDaily <gboys2008@gmail.com>
2024-05-05 16:46:10 -0500

    (Dashboard) Added a better placeholder

--- apps/dashboard/src/lib/components/inputs/role-input.svelte
+++ apps/dashboard/src/lib/components/inputs/role-input.svelte
@@ -70,7 +70,7 @@
       use:melt={$input}
       class="flex h-10 w-[200px] rounded-md border bg-popover p-4 text-popover-foreground shadow-md px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
 
-      placeholder="Role name"
+      placeholder="Search for a role"
     />
   </div>
 </div>
