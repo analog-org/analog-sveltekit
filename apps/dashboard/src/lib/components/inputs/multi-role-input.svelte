b0afa46b7fa0d10af874494d106ebd750bdea2d2
BossDaily <gboys2008@gmail.com>
2024-05-05 16:55:09 -0500

    (Dashboard) Added more dynamic placeholders

--- apps/dashboard/src/lib/components/inputs/multi-role-input.svelte
+++ apps/dashboard/src/lib/components/inputs/multi-role-input.svelte
@@ -81,7 +81,7 @@
     <input
       use:melt={$input}
       class="flex h-10 w-[200px] rounded-md border bg-popover p-4 text-popover-foreground shadow-md px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
-      placeholder="Search for a role"
+      placeholder={ $open ? "Search for a role" : "Select roles" }
     />
   </div>
 </div>
