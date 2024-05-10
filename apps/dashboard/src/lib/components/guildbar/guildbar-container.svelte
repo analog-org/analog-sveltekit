78a03843b513f441e26de581799f89590c4deb7a
BossDaily <gboys2008@gmail.com>
2024-05-09 19:53:18 -0500

    (Dashboard) Component will always overlap any component

--- apps/dashboard/src/lib/components/guildbar/guildbar-container.svelte
+++ apps/dashboard/src/lib/components/guildbar/guildbar-container.svelte
@@ -1,5 +1,5 @@
 <div
-  class="flex flex-col justify-start items-start self-stretch flex-grow-0 relative sticky top-0  flex-shrink-0 overflow-hidden px-[180px] py-5"
+  class="flex flex-col justify-start items-start self-stretch flex-grow-0 relative sticky top-0  flex-shrink-0 overflow-hidden px-[180px] py-5 z-50"
 >
   <slot />
 </div>
